c51e270c644bb730f07bba6a20b1b0428365af1b
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-07-26 21:09:32 +0100

    -p/--part-quiet option.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use winreg::RegKey;
 use winreg::enums::*;
 use std::process::exit;
 
-fn detect_dotnet_version(verbose: bool) -> f32 {
+fn detect_dotnet_version(verbose: u32) -> f32 {
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
     let netp = hklm.open_subkey_with_flags(
     "SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full", KEY_READ).unwrap();
@@ -29,7 +29,7 @@ fn detect_dotnet_version(verbose: bool) -> f32 {
         378389 => ver = 4.5,
         _ => ver = 4.4,
     }
-    if verbose {
+    if verbose > 0 {
         let msg = "Detected .NET Framework =>";
         if ver == 4.4 {
             println!("{} < 4.5", msg);
@@ -44,15 +44,15 @@ fn display_version() {
     exit(0);
 }
 
-fn is_required_dotnet(req: f32, verbose: bool) {
+fn is_required_dotnet(req: f32, verbose: u32) {
     let mut ec = -1;
     let ver = detect_dotnet_version(verbose);
     if ver >= req {
-        if verbose {
+        if verbose > 1 {
             println!("Meets or exceeds required version => {}", req);
             ec = 0;
         }
-    } else if verbose {
+    } else if verbose > 1 {
         println!("Does not meet required version => {}", req);
     }
     exit(ec);
@@ -64,10 +64,11 @@ fn display_usage(program: &str) {
     println!("installed on a Windows system.");
     println!("\nCopyright 2017 Sam Saint-Pettersen.");
     println!("\nReleased under the MIT License.");
-    println!("\nUsage: {} [-h | -v][-r <dotnet version>][-q]", program);
+    println!("\nUsage: {} [-h | -v][-r <dotnet version>][-q | -p]", program);
     println!("\nOptions are:\n");
     println!("-r | --required: The required minimal .NET version.");
     println!("-q | --quiet: Do not output version to stdout; just exit code (0 for pass; -1 for fail).");
+    println!("-p | --part-quiet: Output version to stdout, but not \"Meets...\" message.");
     println!("-h | --help: Display this usage information and exit.");
     println!("-v | --version: Display program version information and exit.");
     exit(0);
@@ -86,12 +87,13 @@ fn main() {
     let cli = CliOptions::new("dotnetdetect");
     let program = cli.get_program();
     let mut required = String::new();
-    let mut verbose = true;
+    let mut verbose = 2;
     if cli.get_num() > 1 {
         for (i, a) in cli.get_args().iter().enumerate() {
             match a.trim() {
                 "-r" | "--required" => required = cli.next_argument(i),
-                "-q" | "--quiet" => verbose = false,
+                "-p" | "--part-quiet" => verbose = 1,
+                "-q" | "--quiet" => verbose = 0,
                 "-h" | "--help" => display_usage(&program),
                 "-v" | "--version" => display_version(),
                 _ => continue,
